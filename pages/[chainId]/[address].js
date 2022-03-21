574df999f1cb2b7d4c1c57de9920d4ec0b0cf799
sharath V <sharathkrml@gmail.com>
2022-03-22 00:20:30 +0530

    added components folder

--- pages/[chainId]/[address].js
+++ pages/[chainId]/[address].js
@@ -2,6 +2,8 @@ import { useRouter } from "next/router";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 export default function NftDrop() {
   const router = useRouter();
+  //to catch something like
+  //http://127.0.0.1:3000/80001/0x3b1b6401a6E2993790D1F4E9D7805DcBcD00323E
   const { chainId, address } = router.query;
   console.log(chainId);
   console.log(address);
